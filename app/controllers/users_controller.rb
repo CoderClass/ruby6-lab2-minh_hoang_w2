b0c79a69516cb4287fa21d5ee1bd6e654edacf81
Hoang Le <xh.dhhv@gmail.com>
2017-02-22 20:29:36 +0700

    milestone 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Welcome #{@user.name}"	
+  		session[:user_id] = @user.id
+  		redirect_to users_path
+  		  		# render 'new'
+  	else
+  		flash[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+  		redirect_to new_user_path
+  	end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :password, :password_confirmation, :name, :image_url)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
   resources :users
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
